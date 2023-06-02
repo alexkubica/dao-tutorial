6f06d4a6bfeb49f1b1023da824d56544ca5c0674
Alex Kubica <alexku@monday.com>
2023-06-02 20:02:47 +0300

    updaet frontend

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -28,7 +28,29 @@ ex : Asssume you have a string and a number to pass
 */
 
 async function main() {
-  // Write your deployment files here
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the KubiDAO contract
+  const KubiDAO = await ethers.getContractFactory("KubiDAO");
+  const kubiDAO = await KubiDAO.deploy(
+    fakeNftMarketplace.address,
+    "0xbb9af75338fb9d424058184fbf0cdddd8d4be01d",
+    {
+      // This assumes your metamask account has at least 1 ETH in its account
+      // Change this value as you want
+      value: ethers.utils.parseEther("0.1"),
+    }
+  );
+  await kubiDAO.deployed();
+
+  console.log("KubiDAO deployed to: ", kubiDAO.address);
 }
 
 // Async Sleep function
